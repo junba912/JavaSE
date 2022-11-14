bc86b2760387167df7853ab32163f5534c897318
이준수 <@192.168.124.120>
2022-11-14 19:39:33 +0900

    첫 커밋

---
+++ src/day02/test/MainClass01.java
@@ -0,0 +1,16 @@
+package day02.test;
+/*
+ * 주석이란?
+ * 	컴퓨터가 코드로 인식하지 않는다.
+ */
+/*
+ * 여러줄 주석
+ */
+//한줄 주석
+public class MainClass01 {
+	//프로그램 시작이 되는 메인메소드
+	public static void main(String[] args) {
+		System.out.println("메인메서드 시작!");
+	}
+
+}
